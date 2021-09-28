45068071a25800e2ad28ce8c61566a192235d80b
wesleymutwiri <wesleymochiemo@gmail.com>
2021-09-28 16:12:45 +0300

    added login page animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,206 @@
 <script>
-  import {onMount} from 'svelte';
-  let count = 0;
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+  let time = new Date();
+  let date = time.toLocaleString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
+  $: hours = time.getHours();
+  $: minutes =
+    time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+  // $: seconds = time.getSeconds();
+  let ready = false;
+  let loginpage = true;
   onMount(() => {
-    const interval = setInterval(() => count++, 1000);
+    setInterval(() => {
+      ready = true;
+    }, 1000);
+
+    const interval = setInterval(() => {
+      time = new Date();
+    }, 1000);
+
     return () => {
       clearInterval(interval);
     };
   });
 </script>
 
-<style>
-  :global(body) {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-  }
-  .App {
-    text-align: center;
-  }
-  .App code {
-    background: #0002;
-    padding: 4px 8px;
-    border-radius: 4px;
-  }
-  .App p {
-    margin: 0.4rem;
-  }
+{#if !ready}
+  <div class="loading-container">
+    <img src="windows-logo.png" alt="" in:fade={{ duration: 400 }} />
+    <div class="loader" transition:fade>
+      <div class="circle" />
+      <div class="circle" />
+      <div class="circle" />
+      <div class="circle" />
+      <div class="circle" />
+    </div>
+  </div>
+{/if}
 
-  .App-header {
-    background-color: #f9f6f6;
-    color: #333;
-    min-height: 100vh;
+{#if ready}
+  {#if loginpage}
+    <div
+      class="App login-animation"
+      transition:fade
+      on:click={() => (loginpage = false)}
+    >
+      <div class="time-container">
+        <h1>
+          {#if hours > 12}
+            {hours - 12}:{minutes} PM
+          {:else}
+            {hours}:{minutes} AM
+          {/if}
+        </h1>
+        <p>
+          {date}
+        </p>
+      </div>
+    </div>
+  {:else}
+    <div class="login-page login-animation">
+      <div class="avatar">
+        <img src="windows-logo.png" alt="" />
+        <h1 class="no-break">
+        Wesley Mutwiri
+        </h1>
+      </div>
+    </div>
+  {/if}
+  <!-- {#if !loginpage}
+    login here
+  {/if} -->
+{/if}
+
+<style>
+  .no-break {
+    word-break: keep-all;
+    white-space: nowrap;
+  }
+  .login-page {
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-    font-size: calc(10px + 2vmin);
   }
-  .App-link {
-    color: #ff3e00;
+  .avatar {
+    /* border-radius: 50%; */
+    width: 100px;
+    height: 100px;
+    margin: auto;
   }
-  .App-logo {
-    height: 36vmin;
-    pointer-events: none;
-    margin-bottom: 3rem;
-    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
+  .avatar img {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
-  @keyframes App-logo-pulse {
-    from {
-      transform: scale(1);
+
+  .time-container {
+    padding-top: 200px;
+    text-align: center;
+  }
+  .time-container h1 {
+    font-size: 5rem;
+  }
+  .time-container p {
+    font-size: 1.5rem;
+  }
+  .login-animation {
+    background: blue;
+    color: #fff;
+    height: 100vh;
+  }
+  .loading-container {
+    background: black;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .loading-container img {
+    display: block;
+    margin: 0 auto;
+    width: 150px;
+    height: 150px;
+  }
+  .loader {
+    position: relative;
+    /* padding-top: 100px; */
+    width: 40px;
+    margin: 200px auto;
+  }
+  .loader .circle {
+    position: absolute;
+    width: 38px;
+    height: 38px;
+    opacity: 0;
+    transform: rotate(225deg);
+    animation-iteration-count: infinite;
+    animation-name: orbit;
+    animation-duration: 5.5s;
+  }
+  .loader .circle:after {
+    content: "";
+    position: absolute;
+    width: 5px;
+    height: 5px;
+    border-radius: 5px;
+    background: #fff;
+    /* Pick a color */
+  }
+  .loader .circle:nth-child(2) {
+    animation-delay: 240ms;
+  }
+  .loader .circle:nth-child(3) {
+    animation-delay: 480ms;
+  }
+  .loader .circle:nth-child(4) {
+    animation-delay: 720ms;
+  }
+  .loader .circle:nth-child(5) {
+    animation-delay: 960ms;
+  }
+  @keyframes orbit {
+    0% {
+      transform: rotate(225deg);
+      opacity: 1;
+      animation-timing-function: ease-out;
     }
-    to {
-      transform: scale(1.06);
+    7% {
+      transform: rotate(345deg);
+      animation-timing-function: linear;
+    }
+    30% {
+      transform: rotate(455deg);
+      animation-timing-function: ease-in-out;
+    }
+    39% {
+      transform: rotate(690deg);
+      animation-timing-function: linear;
+    }
+    70% {
+      transform: rotate(815deg);
+      opacity: 1;
+      animation-timing-function: ease-out;
+    }
+    75% {
+      transform: rotate(945deg);
+      animation-timing-function: ease-out;
+    }
+    76% {
+      transform: rotate(945deg);
+      opacity: 0;
+    }
+    100% {
+      transform: rotate(945deg);
+      opacity: 0;
     }
   }
 </style>
-
-<div class="App">
-  <header class="App-header">
-    <img src="/logo.svg" class="App-logo" alt="logo" />
-    <p>Edit <code>src/App.svelte</code> and save to reload.</p>
-    <p>Page has been open for <code>{count}</code> seconds.</p>
-    <p>
-      <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-        Learn Svelte
-      </a>
-    </p>
-  </header>
-</div>
