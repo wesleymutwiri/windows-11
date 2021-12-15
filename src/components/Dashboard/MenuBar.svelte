b0cc37a42fbab0664694407ee200325916745cc5
wesleymutwiri <wesleymochiemo@gmail.com>
2021-12-15 11:47:40 +0300

    created wifi-components branch

--- src/components/Dashboard/MenuBar.svelte
+++ src/components/Dashboard/MenuBar.svelte
@@ -1,6 +1,7 @@
 <script>
   import Taskbar from "./TaskBar.svelte";
   import Calendar from "./Calendar.svelte";
+  import WindowsUtilityIcons from "./WindowsUtilityIcons.svelte";
 </script>
 
 <footer>
@@ -46,59 +47,7 @@
           ><polyline points="18 15 12 9 6 15" />
         </svg>
       </div>
-      <div class="icon">
-        <svg
-          viewBox="0 0 24 24"
-          width="18"
-          height="18"
-          stroke="currentColor"
-          stroke-width="2"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="css-i6dzq1"
-        >
-          <path d="M5 12.55a11 11 0 0 1 14.08 0" />
-          <path d="M1.42 9a16 16 0 0 1 21.16 0" />
-          <path d="M8.53 16.11a6 6 0 0 1 6.95 0" />
-          <line x1="12" y1="20" x2="12.01" y2="20" />
-        </svg>
-      </div>
-      <div class="icon">
-        <svg
-          viewBox="0 0 24 24"
-          width="18"
-          height="18"
-          stroke="currentColor"
-          stroke-width="2"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="css-i6dzq1"
-          ><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5" /><path
-            d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"
-          /></svg
-        >
-      </div>
-      <div class="icon">
-        <svg
-          viewBox="0 0 24 24"
-          width="18"
-          height="18"
-          stroke="currentColor"
-          stroke-width="2"
-          fill="#fff"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="css-i6dzq1"
-          ><rect x="1" y="6" width="18" height="12" rx="2" ry="2" /><line
-            x1="23"
-            y1="13"
-            x2="23"
-            y2="11"
-          /></svg
-        >
-      </div>
+      <WindowsUtilityIcons />
       <Calendar />
     </div>
   </div>
@@ -107,7 +56,7 @@
 <style>
   .footer-container {
     background: rgba(28, 28, 40, 0.9);
-    height: 39px;
+    height: 42px;
     width: 100%;
     position: fixed;
     bottom: 0;
@@ -128,11 +77,7 @@
     margin-left: auto;
     display: flex;
   }
-  .icon {
-    color: #fff;
-    margin: auto 0.2rem;
-    cursor: pointer;
-  }
+
   img,
   .search {
     height: 33px;
@@ -142,5 +87,10 @@
 
   .search {
     transform: rotate(90deg);
+  }
+  .icon {
+    color: #fff;
+    margin: auto 0.2rem;
+    cursor: pointer;
   }
 </style>
---
+++ src/components/Dashboard/WindowsUtilityIcons.svelte
@@ -0,0 +1,95 @@
+<script>
+  let utilityIconCalled = true;
+</script>
+
+<div
+  class="windows-utility-icons"
+  on:click={() => (utilityIconCalled = !utilityIconCalled)}
+>
+  <div class="icon">
+    <svg
+      viewBox="0 0 24 24"
+      width="18"
+      height="18"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="css-i6dzq1"
+    >
+      <path d="M5 12.55a11 11 0 0 1 14.08 0" />
+      <path d="M1.42 9a16 16 0 0 1 21.16 0" />
+      <path d="M8.53 16.11a6 6 0 0 1 6.95 0" />
+      <line x1="12" y1="20" x2="12.01" y2="20" />
+    </svg>
+  </div>
+  <div class="icon">
+    <svg
+      viewBox="0 0 24 24"
+      width="18"
+      height="18"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="css-i6dzq1"
+      ><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5" /><path
+        d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"
+      /></svg
+    >
+  </div>
+  <div class="icon">
+    <svg
+      viewBox="0 0 24 24"
+      width="18"
+      height="18"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="#fff"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="css-i6dzq1"
+      ><rect x="1" y="6" width="18" height="12" rx="2" ry="2" /><line
+        x1="23"
+        y1="13"
+        x2="23"
+        y2="11"
+      /></svg
+    >
+  </div>
+</div>
+
+{#if utilityIconCalled}
+  <div class="menu-buttons">
+      <div class="grid-icons"></div>
+  </div>
+{/if}
+
+<style>
+  .windows-utility-icons {
+    display: flex;
+    position: relative;
+    padding: 0 0.2rem;
+    cursor: pointer;
+  }
+  .windows-utility-icons:hover {
+    background: hsla(0, 0%, 100%, 0.1);
+  }
+  .icon {
+    color: #fff;
+    margin: auto 0.2rem;
+  }
+  .menu-buttons {
+    position: absolute;
+    top: -365px;
+    background: rgba(36, 36, 36, 0.8);
+    height: 350px;
+    width: 380px;
+    right: 20px;
+    border-radius: 0.4rem;
+    padding: 1rem;
+    color: #fff;
+  }
+</style>
