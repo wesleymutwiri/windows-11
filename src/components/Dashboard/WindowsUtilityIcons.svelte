e5381eead913caf124fbbc5e650c2c32659b5423
wesleymutwiri <wesleymochiemo@gmail.com>
2021-12-16 15:00:34 +0300

    adding various icons

--- src/components/Dashboard/WindowsUtilityIcons.svelte
+++ src/components/Dashboard/WindowsUtilityIcons.svelte
@@ -63,7 +63,90 @@
 
 {#if utilityIconCalled}
   <div class="menu-buttons">
-      <div class="grid-icons"></div>
+    <div class="grid-icons">
+      <div class="utility-icon">
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          stroke-width="2"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="css-i6dzq1"
+          ><path d="M5 12.55a11 11 0 0 1 14.08 0" /><path
+            d="M1.42 9a16 16 0 0 1 21.16 0"
+          /><path d="M8.53 16.11a6 6 0 0 1 6.95 0" /><line
+            x1="12"
+            y1="20"
+            x2="12.01"
+            y2="20"
+          />
+        </svg>
+      </div>
+      <div class="utility-icon">
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          stroke-width="2"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="css-i6dzq1"
+          ><polyline points="6.5 6.5 17.5 17.5 12 23 12 1 17.5 6.5 6.5 17.5" />
+        </svg>
+      </div>
+      <div class="utility-icon">
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          stroke-width="2"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="css-i6dzq1"
+          ><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" /></svg
+        >
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          stroke-width="2"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="css-i6dzq1"
+          ><circle cx="12" cy="12" r="5" /><line
+            x1="12"
+            y1="1"
+            x2="12"
+            y2="3"
+          /><line x1="12" y1="21" x2="12" y2="23" /><line
+            x1="4.22"
+            y1="4.22"
+            x2="5.64"
+            y2="5.64"
+          /><line x1="18.36" y1="18.36" x2="19.78" y2="19.78" /><line
+            x1="1"
+            y1="12"
+            x2="3"
+            y2="12"
+          /><line x1="21" y1="12" x2="23" y2="12" /><line
+            x1="4.22"
+            y1="19.78"
+            x2="5.64"
+            y2="18.36"
+          /><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /></svg
+        >
+      </div>
+      <div class="utility-icon" />
+    </div>
   </div>
 {/if}
 
@@ -91,5 +174,11 @@
     border-radius: 0.4rem;
     padding: 1rem;
     color: #fff;
+  }
+
+  .grid-icons {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
   }
 </style>
